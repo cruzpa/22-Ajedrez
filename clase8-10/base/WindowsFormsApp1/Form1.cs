d77d23cec5b851a586d2698b8dbe87600a774bd7
Cruzpa <cruzpa95@gmail.com>
2025-10-08 21:02:29 -0300

    Add files via upload

---
+++ clase8-10/base/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        BE.Producto producto;
+        BLL.Producto gestor = new BLL.Producto();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Enlazar();
+        }
+
+        public void Enlazar()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = gestor.Listar();
+
+        }
+    }
+}
