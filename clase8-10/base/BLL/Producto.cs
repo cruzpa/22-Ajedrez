b05a7b0668f34a63fd94ed147694fd3192721974
Cruzpa <cruzpa95@gmail.com>
2025-10-08 21:04:29 -0300

    Add files via upload

---
+++ clase8-10/base/BLL/Producto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL
+{
+    public class Producto
+    {
+        DAL.MP_PRODUCTO mp = new DAL.MP_PRODUCTO();
+        public void Grabar(BE.Producto producto)
+        {
+            if (producto.Id == 0)
+            {
+                mp.Insertar(producto);
+            } else
+            {
+                mp.Editar(producto);
+            }
+        }
+
+        public void Borrar(BE.Producto producto)
+        {
+            mp.Borrar(producto);
+        }
+
+        public List<BE.Producto> Listar()
+        {
+            return mp.Listar();
+        }
+    }
+}
